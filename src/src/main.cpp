d99cd19ed3aa3f358e54cad4d5d1d9685ec645b3
Banking simulations <ladimp11@gmail.com>
2026-02-10 15:05:38 +0300

    Update main.cpp

--- src/src/main.cpp
+++ src/src/main.cpp
@@ -1,1 +1,34 @@
+#include "Database.h"
+#include <iostream>
+
+int main()
+{
+    Database db("bank.db");
+    db.init();
+
+    int choice;
+    std::string name;
+    double balance;
+
+    while (true)
+    {
+        std::cout << "\n1. Add account\n2. List accounts\n0. Exit\n> ";
+        std::cin >> choice;
+
+        if (choice == 0) break;
+
+        if (choice == 1)
+        {
+            std::cout << "Owner name: ";
+            std::cin >> name;
+            std::cout << "Initial balance: ";
+            std::cin >> balance;
+            db.addAccount(name, balance);
+        }
+        else if (choice == 2)
+        {
+            db.listAccounts();
+        }
+    }
+}
 
